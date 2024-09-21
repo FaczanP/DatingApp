b369662c37e18961c11b1c9b61ed096bda1a156f
FaczanP <mscf2006@gmail.com>
2024-09-21 18:23:56 +0200

    End of section 9

---
+++ Client/src/app/_services/members.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Member } from '../_models/member'
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  
+
+  constructor(private http: HttpClient) { }
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users')  //, this.getHttpOptions())
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username) //, this.getHttpOptions())
+
+  }
+
+  // getHttpOptions() {
+  //   const userString = localStorage.getItem('user');
+  //   if (!userString) return;
+  //   const user = JSON.parse(userString);
+  //   const header = {
+  //     headers: new HttpHeaders({
+  //       Authorization: 'Bearer ' + user.token
+  //     })
+  //   }
+  //   return header
+  // }
+}
---
+++ Client/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Member } from '../../_models/member';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrl: './member-card.component.css',
+  // encapsulation: ViewEncapsulation.None
+})
+export class MemberCardComponent implements OnInit{
+
+@Input() member: Member | undefined;
+
+  ngOnInit(): void {
+  }
+
+}
